a3753aa387a2b601bec7d7c6550eee82a6228b1d
Tamir Reiss <tamir321@gmail.com>
2020-11-21 09:58:39 +0200

    add tests

---
+++ test/firstTests.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect;
+
+const sum = (...args) => [...args].reduce((a, b) => a + b, 0);
+
+describe('first Tests', function() {
+
+  context('should return 6', function() {
+    it('three arguments', function() {
+     
+     
+      expect(sum(1,2,3)).to.equal(6)
+    })
+
+    specify('two arguments', function() {
+      const a =3
+      const b =6 
+      
+      expect(sum(a,b)).to.equal(6)
+    })
+  })
+    context('without arguments', function() {
+      it('should return 0', function() {
+        expect(sum()).to.equal(0)
+      })
+    })
+})
